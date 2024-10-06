99d577fc18b37ba3c0ed9cd456ba74ecfcab67ad
Avinash445k <avinash0003041997@gmail.com>
2024-10-06 23:11:35 +0530

    github repo push

---
+++ built.sh
@@ -0,0 +1,14 @@
+#1/bin/bash 
+
+# # built 
+type -a pwd 
+type -a kill 
+type -a cd 
+type -a exit 
+
+# # command 
+# type -a shutrdown 
+# type -a ps 
+# type -a adduser 
+# type -a rm 
+# type -a clear
